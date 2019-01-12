62d2ee129240993d9374f10a473fce62d293537f
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-11 20:00:13 -0600

    server.js code finished

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const app = express();
 // const path = require ('path');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
-// DEFINE MIDDLEWARE AND STATIC ASSETS
+// MIDDLEWARE AND STATIC ASSETS
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+    // STATIC
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// API ROUTES 
+// API ROUTES
+app.use("/api", apiRoutes);
 
-// MONGO DB CONNECT 
+// SEND REQUEST FUNCTION
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-// START API SERVER 
+// LISTEN FUNCTION 
+app.listen(PORT, function() {
+  console.log(`The server is now listening on: ${PORT}!`);
+});
+
