cec77f2bf8bf14af629596ceef58581accbe75e6
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-11 19:03:40 -0600

    dependencies syntax fix

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import navigation from './components/navigation';
-import books from './info/books';
-import synposis from './info/synposis';
+import Nav from './components/Nav';
+import Book from './info/Book';
+import Synposis from './info/Synposis';
 import NotFound from './info/NotFound';
 
 // APP FUNCTION 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
+//EXPORT
 export default App;
