29d5925054a71f86678d06e91fb800fd234b9c0f
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-11 18:09:57 -0600

    pseudo code for server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// DEPENDENCIES 
+const express = require('express');
+const app = express();
+// const path = require ('path');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const PORT = process.env.PORT || 8080;
+
+// DEFINE MIDDLEWARE AND STATIC ASSETS
+
+// API ROUTES 
+
+// MONGO DB CONNECT 
+
+// START API SERVER 
